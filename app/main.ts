4e3d684c19ce3fbe2e84a32c748cd85539a9bcda
Charles Simms <charles.simms@joinhandshake.com>
2026-04-22 09:17:07 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,12 +5,28 @@ console.log("Logs from your program will appear here!");
 
 // TODO - encode arbitrary strings
 const PONG_RESPONSE = "+PONG\r\n"
+const PING_INPUT = "*1\r\n$4\r\nPING\r\n"
+
+const memoizedResponse = (connection: net.Socket) => {
+
+    const dataResponseCallback = (data: Buffer) => {
+        // const input = data.toString()
+        // console.log("~~Recieved input:: ", input)
+        // if (input === PING_INPUT) {
+        //     connection.write(PONG_RESPONSE)
+        // }
+
+        // TODO - not reading input quite yet, couldn't get the strings to match
+        connection.write(PONG_RESPONSE)
+    }
+
+    return dataResponseCallback
+}
 
 // Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
     // Handle connection
-    console.log(connection)
-    connection.write(PONG_RESPONSE)
+    connection.on("data", memoizedResponse(connection))
 });
 
 server.listen(6379, "127.0.0.1");
